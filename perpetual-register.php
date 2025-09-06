2f0cc649b82103f6a682e50d48d54aa822b6c6e3
Saugat Thapa <50352881+vileroze@users.noreply.github.com>
2025-09-06 15:11:40 +0545

    enqueued scripts, created admin page available from settings

--- perpetual-register.php
+++ perpetual-register.php
@@ -4,6 +4,7 @@
  * Description: A plugin to manage a perpetual register with CSV data import functionality.
  * Version: 1.0.0
  * Author: WP Creative
+ * Author URI: https://wpcreative.com.au
  * Text Domain: perpetual-register
  */
 
@@ -26,6 +27,11 @@ class PerpetualRegister {
         
         register_activation_hook(__FILE__, array($this, 'activate'));
         register_deactivation_hook(__FILE__, array($this, 'deactivate'));
+
+        add_action('admin_menu', array($this, 'add_admin_menu'));
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
+
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_public_scripts'));
     }
 
     public function activate() {
@@ -52,6 +58,71 @@ class PerpetualRegister {
         
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
+    }
+
+    public function add_admin_menu() {
+        add_options_page(
+            __('Perpetual Register Manager', 'perpetual-register'),
+            __('Perpetual Register Manager', 'perpetual-register'),
+            'manage_options',
+            'perpetual-register',
+            array($this, 'admin_page')
+        );
+    }
+
+    public function enqueue_admin_scripts($hook) {
+        if ($hook !== 'settings_page_perpetual-register') {
+            return;
+        }
+        
+        wp_enqueue_script('jquery');
+        wp_enqueue_script(
+            'ppr-admin-js',
+            PPR_PLUGIN_URL . 'assets/admin/admin.js',
+            array('jquery'),
+            PPR_VERSION,
+            true
+        );
+        
+        wp_enqueue_style(
+            'ppr-admin-css',
+            PPR_PLUGIN_URL . 'assets/admin/admin.css',
+            array(),
+            PPR_VERSION
+        );
+        
+        wp_localize_script('ppr-admin-js', 'ppr_ajax', array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('ppr_nonce')
+        ));
+    }
+
+
+    public function enqueue_public_scripts() {
+        wp_enqueue_script('jquery');
+        wp_enqueue_script(
+            'ppr-public-js',
+            PPR_PLUGIN_URL . 'assets/public/public.js',
+            array('jquery'),
+            PPR_VERSION,
+            true
+        );
+        
+        wp_enqueue_style(
+            'ppr-public-css',
+            PPR_PLUGIN_URL . 'assets/public/public.css',
+            array(),
+            PPR_VERSION
+        );
+    }
+
+
+    public function admin_page() {
+        ?>
+        <div>
+            <h1><?php _e('Perpetual Data Manager', 'perpetual-register'); ?></h1>
+        </div>
+        <?php
     }
 
 }
