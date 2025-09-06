97b4762b71a2ab57b52e31e5ca7340e2d306863e
Saugat Thapa <50352881+vileroze@users.noreply.github.com>
2025-09-06 14:14:21 +0545

    first commit and setup
    - created plugin file structure
    - added plugin constants

---
+++ perpetual-register.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Perpetual Register
+ * Description: A plugin to manage a perpetual register with CSV data import/export functionality.
+ * Version: 1.0.0
+ * Author: WP Creative
+ * Text Domain: perpetual-register
+ */
+
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('PPR_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PPR_VERSION', '1.0.0');
